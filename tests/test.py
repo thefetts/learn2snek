7e5192ee2de6cdd7ca2f604ba2fe889a7e100f43
Jordan Fallon <thefetts@gmail.com>
2017-04-14 15:55:03 -0600

    Add some unit tests to figure out what modules are

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+import unittest
+from code import something
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+    def test_what_am_i_doing(self):
+        self.assertEqual(something.foo(), 5)
+
+if __name__ == '__main__':
+    unittest.main()
