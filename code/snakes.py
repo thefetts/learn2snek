d0cc1bfe6e10635932463e88b4041f2b39b9fbf0
Jordan Hagan <Jordan.Hagan@gmail.com>
2017-04-21 13:43:00 -0600

    Update snakes.py

--- code/snakes.py
+++ code/snakes.py
@@ -4,3 +4,11 @@
 # Scarlet King snakes are harmless, and can be recognized by red stripes touching black stripes
 def is_venomous(snake):
     return 'Snakes???'
+
+def snake_name(i_haz_sign)
+    if i_haz_sign == 'I am a Coral Snake':
+        return 'venomous'
+    elif i_haz_sign == 'I am a Scarlet King':
+        return 'harmless'
+    else:
+        return 'DUNNO!!'
