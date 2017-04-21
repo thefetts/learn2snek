5e97de770f816f2bc97871d9b23e474f85443318
Jordan Fallon <thefetts@gmail.com>
2017-04-21 11:16:10 -0600

    Add some snek tests!

---
+++ code/snakes.py
@@ -0,0 +1,6 @@
+
+
+# Coral snakes are venomous, and can be recognized by red stripes touching yellow stripes
+# Scarlet King snakes are harmless, and can be recognized by red stripes touching black stripes
+def is_venomous(snake):
+    return 'Snakes???'
---
+++ tests/test_snakes.py
@@ -0,0 +1,39 @@
+import unittest
+from code import snakes
+
+
+# Here are a bunch of test cases for finding venomous snakes!
+# I haven't solved this, so I might have all sorts of problems with these tests, who knows!
+# I commented all of the other tests and assertions out so you can work iteratively
+# Get the first assertEqual to pass, then uncomment the next line, making it pass before moving on
+# Once a whole test is working, uncomment the next def line and the first assertEqual and carry on!
+class TestSnakes(unittest.TestCase):
+    def test_is_venomous_finds_coral_snakes(self):
+        # Any snake with red touching yellow on either side is deadly
+        # Some of these aren't real Coral snake configurations, but that's ok for today :)
+
+        self.assertEqual(snakes.is_venomous(['red', 'yellow']), "You're a dead fellow!")
+        # self.assertEqual(snakes.is_venomous(['black', 'yellow', 'red', 'yellow']), "You're a dead fellow!")
+        # self.assertEqual(snakes.is_venomous(['yellow', 'red', 'yellow', 'black']), "You're a dead fellow!")
+        # self.assertEqual(snakes.is_venomous(['black', 'red', 'red', 'yellow']), "You're a dead fellow!")
+
+    # def test_is_venomous_finds_scarlet_king_snakes(self):
+        # Any snake with red touching black on either side is fine
+        # Some of these aren't real Scarlet King snake configurations, but that's ok for today :)
+
+        # self.assertEqual(snakes.is_venomous(['black', 'red']), "You're ok Jack!")
+        # self.assertEqual(snakes.is_venomous(['black', 'yellow', 'black', 'red']), "You're ok Jack!")
+        # self.assertEqual(snakes.is_venomous(['red', 'black', 'yellow', 'black']), "You're ok Jack!")
+        # self.assertEqual(snakes.is_venomous(['yellow', 'black', 'red']), "You're ok Jack!")
+
+    # def test_is_venomous_finds_invalid_snakes(self):
+        # Bad snakes are anything that don't fit in the above two rules
+
+        # self.assertEqual(snakes.is_venomous(['green', 'red', 'orange', 'black']), "BAD SNEK!")
+        # self.assertEqual(snakes.is_venomous([]), "BAD SNEK!")
+        # self.assertEqual(snakes.is_venomous(['RED RED RED RED RED RED JUST A RED SNAKE']), "BAD SNEK!")
+        # self.assertEqual(snakes.is_venomous(['yellow', 'black', 'yellow', 'black']), "BAD SNEK!")
+
+
+if __name__ == '__main__':
+    unittest.main()
