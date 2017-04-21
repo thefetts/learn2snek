9b4fc9d0a47b6b5512d92ebcd3fbf21804805b0d
Jordan Hagan <Jordan.Hagan@gmail.com>
2017-04-21 14:33:15 -0600

    Update snakes.py
    playing with snek

--- code/snakes.py
+++ code/snakes.py
@@ -5,10 +5,36 @@
 def is_venomous(snake):
     return 'Snakes???'
 
-def snake_name(i_haz_sign)
+animals = ['cat','bird','snake','dog','horse','goose','Jon Bon Jovi']
+# I am 99% sure this very wrong.  But I can't run it like I want to
+def is_animal(animals):
+    if 'snake' in animals:
+        return 'Snake'
+    elif animals.index >= 0:
+        return 'Not Snake'
+    else:
+        return 'Not Animal'
+
+def snake_name(i_haz_sign):
     if i_haz_sign == 'I am a Coral Snake':
         return 'venomous'
     elif i_haz_sign == 'I am a Scarlet King':
         return 'harmless'
     else:
         return 'DUNNO!!'
+    
+def pretty_snake(color1, color2):
+    if color1 + '-' + color2 == 'red-yellow':
+        return 'venomous'
+    elif color1 + '-' + color2 == 'red-black':
+        return 'harmless'
+    else:
+        return 'STILL DUNNO!!'
+    
+def is_it_snek(animals, i_haz_sign, color1, color2):
+    return (is_animal(animals)+','+snake_name(i_haz_sign)+','+pretty_snake(color1, color2))
+
+#this does not work like I want it to yet....
+results_list=[is_it_snek('snake','I am a Coral Snake','red','black')]
+print results_list
+    
