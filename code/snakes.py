b5674488d87bdcb254fb716017fc28a7b30db33f
Jordan Fallon <thefetts@gmail.com>
2017-04-21 18:45:12 -0600

    fixed sneks

--- code/snakes.py
+++ code/snakes.py
@@ -2,15 +2,101 @@
 
 # Coral snakes are venomous, and can be recognized by red stripes touching yellow stripes
 # Scarlet King snakes are harmless, and can be recognized by red stripes touching black stripes
-def is_venomous(snake):
-    return 'Snakes???'
+
+
+# def is_venomous(colors):
+#     red_index = colors.index('red') if 'red' in colors else -1
+#     if red_index > -1:
+#         if (red_index < len(colors)-1 and colors[red_index + 1] == 'yellow') or (red_index > 0 and colors[red_index - 1] == 'yellow'):
+#             return 'You\'re a dead fellow!'
+#         elif (red_index < len(colors)-1 and colors[red_index + 1] == 'black') or (red_index > 0 and colors[red_index - 1] == 'black'):
+#             return 'You\'re ok Jack!'
+#
+#     return 'BAD SNEK!'
+
+def is_venomous(colors):
+    red_index = colors.index('red') if 'red' in colors else -1
+    if red_index > -1:
+        if before_or_after_red('yellow', red_index, colors):
+            return 'You\'re a dead fellow!'
+        elif before_or_after_red('black', red_index, colors):
+            return 'You\'re ok Jack!'
+
+    return 'BAD SNEK!'
+
+
+def before_or_after_red(color, red_index, colors):
+    return (red_index < len(colors)-1 and colors[red_index + 1] == color) or \
+           (red_index > 0 and colors[red_index - 1] == color)
+
+
+
+
+
+
+class Snake:
+    def __init__(self, colors):
+        self.colors = colors
+        self.red_index = colors.index('red') if 'red' in colors else -1
+
+    def is_venomous(self):
+        if self.red_index > -1:
+            if self.before_or_after_red('yellow'):
+                return 'You\'re a dead fellow!'
+            elif self.before_or_after_red('black'):
+                return 'You\'re ok Jack!'
+
+        return 'BAD SNEK!'
+
+
+    def before_or_after_red(self, color):
+        return (self.red_index < len(self.colors)-1 and self.colors[self.red_index + 1] == color) or \
+               (self.red_index > 0 and self.colors[self.red_index - 1] == color)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 animals = ['cat','bird','snake','dog','horse','goose','Jon Bon Jovi']
 # I am 99% sure this very wrong.
-def is_animal(animals):
-    if 'snake' in animals:
+def is_animal(animal):
+    if animal == 'snake':
         return 'Snake'
-    elif animals.index >= 0:
+    elif animal in animals:
         return 'Not Snake'
     else:
         return 'Not Animal'
@@ -31,10 +117,11 @@ def pretty_snake(color1, color2):
     else:
         return 'STILL DUNNO!!'
     
-def is_it_snek(animals, i_haz_sign, color1, color2):
-    return (is_animal(animals)+','+snake_name(i_haz_sign)+','+pretty_snake(color1, color2))
+def is_it_snek(animal, i_haz_sign, color1, color2):
+    return [is_animal(animal), snake_name(i_haz_sign), pretty_snake(color1, color2)]
 
 #this does not work like I want it to yet....
-results_list=[is_it_snek('snake','I am a Coral Snake','red','black')]
-print results_list
+results_list=is_it_snek('poo','I am a Coral Snake','red','black')
+print(results_list)
+
     
--- tests/test_snakes.py
+++ tests/test_snakes.py
@@ -1,5 +1,6 @@
 import unittest
 from code import snakes
+Snake = snakes.Snake
 
 
 # Here are a bunch of test cases for finding venomous snakes!
@@ -12,27 +13,28 @@ class TestSnakes(unittest.TestCase):
         # Any snake with red touching yellow on either side is deadly
         # Some of these aren't real Coral snake configurations, but that's ok for today :)
 
-        self.assertEqual(snakes.is_venomous(['red', 'yellow']), "You're a dead fellow!")
-        # self.assertEqual(snakes.is_venomous(['black', 'yellow', 'red', 'yellow']), "You're a dead fellow!")
-        # self.assertEqual(snakes.is_venomous(['yellow', 'red', 'yellow', 'black']), "You're a dead fellow!")
-        # self.assertEqual(snakes.is_venomous(['black', 'red', 'red', 'yellow']), "You're a dead fellow!")
+        # One example using the Snake class and object-oriented programming
+        self.assertEqual(Snake(['red', 'yellow']).is_venomous(), "You're a dead fellow!")
 
-    # def test_is_venomous_finds_scarlet_king_snakes(self):
+        # The rest of the examples use functional programming
+        self.assertEqual(snakes.is_venomous(['black', 'yellow', 'red', 'yellow']), "You're a dead fellow!")
+        self.assertEqual(snakes.is_venomous(['black', 'yellow', 'red']), "You're a dead fellow!")
+
+    def test_is_venomous_finds_scarlet_king_snakes(self):
         # Any snake with red touching black on either side is fine
         # Some of these aren't real Scarlet King snake configurations, but that's ok for today :)
 
-        # self.assertEqual(snakes.is_venomous(['black', 'red']), "You're ok Jack!")
-        # self.assertEqual(snakes.is_venomous(['black', 'yellow', 'black', 'red']), "You're ok Jack!")
-        # self.assertEqual(snakes.is_venomous(['red', 'black', 'yellow', 'black']), "You're ok Jack!")
-        # self.assertEqual(snakes.is_venomous(['yellow', 'black', 'red']), "You're ok Jack!")
+        self.assertEqual(snakes.is_venomous(['red', 'black']), "You're ok Jack!")
+        self.assertEqual(snakes.is_venomous(['black', 'red', 'black', 'yellow']), "You're ok Jack!")
+        self.assertEqual(snakes.is_venomous(['yellow', 'black', 'red']), "You're ok Jack!")
 
-    # def test_is_venomous_finds_invalid_snakes(self):
+    def test_is_venomous_finds_invalid_snakes(self):
         # Bad snakes are anything that don't fit in the above two rules
 
-        # self.assertEqual(snakes.is_venomous(['green', 'red', 'orange', 'black']), "BAD SNEK!")
-        # self.assertEqual(snakes.is_venomous([]), "BAD SNEK!")
-        # self.assertEqual(snakes.is_venomous(['RED RED RED RED RED RED JUST A RED SNAKE']), "BAD SNEK!")
-        # self.assertEqual(snakes.is_venomous(['yellow', 'black', 'yellow', 'black']), "BAD SNEK!")
+        self.assertEqual(snakes.is_venomous(['green', 'red', 'orange', 'black']), "BAD SNEK!")
+        self.assertEqual(snakes.is_venomous([]), "BAD SNEK!")
+        self.assertEqual(snakes.is_venomous(['RED RED RED RED RED RED JUST A RED SNAKE']), "BAD SNEK!")
+        self.assertEqual(snakes.is_venomous(['yellow', 'black', 'yellow', 'black']), "BAD SNEK!")
 
 
 if __name__ == '__main__':
