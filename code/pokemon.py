c4ac7afeafc122c60e8fdcff07526d1cc5e83a9a
Jordan Fallon <thefetts@gmail.com>
2017-04-24 21:25:35 -0600

    Pokemon Johto!

--- code/pokemon.py
+++ code/pokemon.py
@@ -1,2 +1,66 @@
+import copy
+
+data = {
+    'pokedex': {
+        'Pikachu': {
+            'health': 100,
+            'power': 50
+        },
+
+        'Butterfree': {
+            'health': 60,
+            'power': 40
+        },
+
+        'Charizard': {
+            'health': 110,
+            'power': 70
+        },
+
+        'Mew': {
+            'health': 500,
+            'power': 100
+        },
+
+        'Mewtwo': {
+            'health': 500,
+            'power': 100
+        },
+
+        'Oddish': {
+            'health': 0,
+            'power': 100
+        }
+    }
+}
+
+
 def attack(offense, defense):
-    return None
+    combat_log = []
+    pika_power = data['pokedex'][offense]['power']
+    ouchies = data['pokedex'][defense]['health'] - pika_power
+    data['pokedex'][defense]['health'] = ouchies
+    combat_log.append(offense + ' hits ' + defense + ' for ' + str(pika_power) + '!')
+    if ouchies > 0:
+        combat_log.append(defense + ' has ' + str(ouchies) + ' HP remaining.')
+    else:
+        combat_log.append(defense + ' has fainted!')
+    return combat_log
+
+
+def battle(offense, defense):
+    pokedex_copy = copy.deepcopy(data['pokedex'])
+    if data['pokedex'][defense]['health'] <= 0:
+        return [defense + ' has fainted!']
+    elif data['pokedex'][offense]['health'] <= 0:
+        return [offense + ' has fainted!']
+    else:
+        results = []
+        while data['pokedex'][offense]['health'] > 0:
+            results += attack(offense, defense)
+            if data['pokedex'][defense]['health'] > 0:
+                results += attack(defense, offense)
+            else:
+                break
+        data['pokedex'] = pokedex_copy
+        return results
--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -1,24 +1,60 @@
 import unittest
+
 from code import pokemon
 
 
 class TestSnakes(unittest.TestCase):
     def test_pokemon_can_deal_damage(self):
-        pikachu = {
-            'health': 100,
-            'power': 50,
-            'name': 'Pikachu'
-        }
+        result = pokemon.attack('Pikachu', 'Butterfree')
+        self.assertEqual(result, [
+            'Pikachu hits Butterfree for 50!',
+            'Butterfree has 10 HP remaining.'
+        ])
 
-        butterfree = {
-            'health': 50,
-            'power': 40,
-            'name': 'Butterfree'
-        }
+    def test_pokemon_can_faint(self):
+        result = pokemon.attack('Charizard', 'Butterfree')
+        self.assertEqual(result, [
+            'Charizard hits Butterfree for 70!',
+            'Butterfree has fainted!'
+        ])
 
-        result = pokemon.attack(pikachu, butterfree)
-        self.assertEqual(result, 'Pikachu hits Butterfree for 50!')
-
+    def test_pokemon_battle_lists_both_attacks(self):
+        result = pokemon.battle('Pikachu', 'Butterfree')
+        self.assertEqual(result, [
+            'Pikachu hits Butterfree for 50!',
+            'Butterfree has 10 HP remaining.',
+            'Butterfree hits Pikachu for 40!',
+            'Pikachu has 60 HP remaining.',
+            'Pikachu hits Butterfree for 50!',
+            'Butterfree has fainted!'
+        ])
+    def test_epic_pokemon(self):
+        result = pokemon.battle('Mew','Mewtwo')
+        self.assertEqual(result, [
+            'Mew hits Mewtwo for 100!',
+            'Mewtwo has 400 HP remaining.',
+            'Mewtwo hits Mew for 100!',
+            'Mew has 400 HP remaining.',
+            'Mew hits Mewtwo for 100!',
+            'Mewtwo has 300 HP remaining.',
+            'Mewtwo hits Mew for 100!',
+            'Mew has 300 HP remaining.',
+            'Mew hits Mewtwo for 100!',
+            'Mewtwo has 200 HP remaining.',
+            'Mewtwo hits Mew for 100!',
+            'Mew has 200 HP remaining.',
+            'Mew hits Mewtwo for 100!',
+            'Mewtwo has 100 HP remaining.',
+            'Mewtwo hits Mew for 100!',
+            'Mew has 100 HP remaining.',
+            'Mew hits Mewtwo for 100!',
+            'Mewtwo has fainted!'
+        ])
+def test_sick_pokemon(self):
+        result = pokemon.battle('Mew','Oddish')
+        self.assertEqual(result, [
+            'Oddish has fainted!'
+        ])
 
 if __name__ == '__main__':
     unittest.main()
